8018b6c44b919bd331963f77d15939875c4926ea
mbozecki <mboz@interia.pl>
2021-06-23 14:13:47 +0200

    Dodawanie uzytkownikow, i wyswietlanie ich w panelu Admina

--- admin/add-user.php
+++ admin/add-user.php
@@ -1,33 +1,34 @@
 <?php include('partials/menu.php'); ?>
 
+
     <div class="main-content">
     <div class="wrapper">
-    <h1> Dodaj uzytkownika</h1>
+    <h1> Zarejestruj sie</h1>
     <form action="" method="POST">
         <table>
             <tr>
                 <td>Email: </td>
                 <td>
-                    <input type="text" name="full_name" placeholder="Wprowadz swoj email">
+                    <input type="text" name="email" placeholder="Wprowadz swoj email">
                 </td>
             </tr>
             <tr>
                 <td>Hasło: </td>
                 <td>
-                    <input type="password" name="full_name" placeholder="Wprowadz swoje hasło">
+                    <input type="password" name="password" placeholder="Wprowadz swoje hasło">
                 </td>
             </tr>
             <tr>
                 <td>Imię: </td>
                 <td>
-                    <input type="text" name="full_name" placeholder="Wprowadz swoje imięl">
+                    <input type="text" name="name" placeholder="Wprowadz swoje imięl">
                 </td>
             </tr>
 
             <tr>
             <td colspan="2"></td>
                 <td>
-                    <input type="submit" name="submit" value="Users" placeholder="btn">
+                    <input type="submit" name="submit" value="Dodaj!" placeholder="btn">
                 </td>
             </tr>
         </table>
@@ -36,3 +37,45 @@
     </div></div>
 
 <?php include('partials/footer.php'); ?>
+
+<?php
+
+    //sprawdz czy przycisk klikniety
+    if(isset($_POST['submit']))
+    {
+        $email = $_POST['email'];
+        $password= $_POST['password'];
+        $name= $_POST['name'];
+        
+
+        //stworz zapytanie do bazy
+        $sql= "INSERT INTO users SET
+        email= '$email',
+        password= '$password',
+        fname= '$name'   ";
+
+        //polacz sie z baza
+        $res = mysqli_query($conn, $sql) or die(mysqli_error($conn));
+
+        //sprawdz czy zapytanie sie powiodlo
+        if ($res==TRUE)
+        {
+            //echo "Zapytanie udalo sie";
+            $_SESSION['add']= "Dodano uzytkownika";
+            //przekieruj
+            header("location:".SITEURL.'admin/manage-user.php');
+
+            
+        }
+         else {
+           // echo "Zapytanie nie udalo sie";
+           $_SESSION['add']= "Nie udalo sie dodac uzytkownika";
+            //przekieruj
+            header("location:".SITEURL.'admin/manage-user.php');
+        }
+    }
+    else{
+
+    }
+
+?>
--- admin/manage-user.php
+++ admin/manage-user.php
@@ -6,19 +6,45 @@
             <a href="#" class="btn-primary">Dodaj uzytkownika</a>
                 <table class="width100">
                     <tr>
+                        <td> ID</th>
+                        <th>Imię</th>
                         <th>E-mail</th>
-                        <th>Hasło</th>
-                        <th>Pełne imię</th>
                         <th>Admin?</th>
                         <th>Akcje</th>
                     </tr>
-                    <tr>
-                        <td>sasd</td>
-                        <td>Hasdło</td>
-                        <td>Pasdsadimię</td>
-                        <td>A2n?</td>
-                        <td><a href="#" class="btn-add">Zaktualizuj</a> <a href="#" class="btn-delete">Usun</a></td>
-                    </tr>
+
+                    <?php
+                        $sql= "SELECT * FROM users";
+                        $res= mysqli_query($conn, $sql);
+
+                        if($res==TRUE)
+                        {
+                            $count= mysqli_num_rows($res);
+                            
+                            if ($count > 0)
+                            {
+                                while($rows=mysqli_fetch_assoc($res))
+                                {
+                                    $id= $rows['user_id'];
+                                    $fname=$rows['fname'];
+                                    $email= $rows['email'];
+                                    $isAdmin= $rows['isAdmin'];
+                                }
+
+                                ?>
+
+                                <tr>
+                                    <td><?php echo $id; ?></td>
+                                    <td><?php echo $fname; ?></td>
+                                    <td><?php echo $email; ?></td>
+                                    <td><?php echo ($isAdmin==1 ? "TAK" : "NIE"); ?>
+                                    <td><a href="#" class="btn-add">Zaktualizuj</a> <a href="#" class="btn-delete">Usun</a></td>
+                                </tr>
+                                <?php
+                            }
+                        }
+                    ?>
+
                 </table>
             </div>
             
